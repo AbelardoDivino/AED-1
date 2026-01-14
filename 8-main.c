703f21797960473cb58b9a5e38bf6c0712d152e5
AbelardoDivino <aaaaaaaaaaasafafe@gmail.com>
2026-01-14 20:16:04 -0300

    .

--- 8-main.c
+++ 8-main.c
@@ -1,87 +1,109 @@
-// #include <stdio.h>
-// #include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 
-// #define maxtam 20
+#define maxtam 20
 
-// typedef int Apontador;
+typedef  int Apontador;
 
-// typedef struct
-// {
-//     int pessoa[maxtam];
-// } tipoitem;
-
-// typedef struct
-// {
-//     tipoitem pessoa;
-//     Apontador frente;
-//     Apontador tras;
-// } tipofila;
-
-// // void inicializar(tipofila *f)
-// // {
-
-// //     f->frente = 0;
-// //     f->tras = 0;
-// // }
-
-// void cadastrar(tipofila *f, tipoitem *item)
-// {
-
-//     if ((f->tras + 1) % maxtam == f->frente)
-//     {
-//         printf("fila esta cheia");
-//     }
-
-//     else{
-
-//         printf("digite as para cadastrar\n");
-//         scanf("%d", &item->pessoa[f->tras]);
-
-//         f->tras = f->tras + 1 % maxtam;
+typedef struct 
+{
+    int pessoas[maxtam];
+    Apontador frente;
+    Apontador tras;
+    int qnt;
+}tipofila;
 
 
-//     }
+void inicializar(tipofila *f){
 
+    f->frente = 0;
+    f->tras = 0;
+    f->qnt = 0;
+}
 
-// }
+void inserir(tipofila *f, int pessoas){
 
-// void atender(tipofila *f){
-
-//     if (f->frente == f->tras)
-//     {
-//         printf("fila esta vazia \n");
-//     }
-//     else{
-//         f->tras --;
-//     }
+    if (f->qnt >= maxtam)
+    {
+        printf("fila esta cheia\n");
+        return;
+    }
+    else{
+        f->pessoas[f->tras] = pessoas;
+        f->tras = (f->tras + 1) % maxtam;
+        f->qnt++;
+    }
     
 
-// }
+}
 
-// void proximo(tipofila *f){
+void atender(tipofila *f){
 
-//     if (f->frente == f->tras)
-//     {
-//         printf("fila esta vazia \n");
-//     }
-//     else{
-//         printf(" o prixmo da fila sera \n", f->frente +1);
-//     }
+    if (f->qnt == 0)
+    {
+        printf("fila esta vazia ERRO\n");   
+        return;
+    }
+    else{
+       printf(" Atendendo cliente:%d\n", f->pessoas[f->frente]);
+       f->frente = (f->frente  + 1) % maxtam;
+       f->qnt--;
+    }
+
+}
+
+void proximo(tipofila *f){
+
+    if(f->qnt == 0){
+        printf("fila vazia Erro\n");
+    }
+    else{
+            printf("o proximo sera: %d\n", f->pessoas[f->frente]);
+    }
+
+
+}
+
+int main(){
+
+    tipofila fila;
+
+    inicializar(&fila);
+
+    int p, opcao;
+
+
+    do
+    {
+        printf("1 -digite um para enserir\n");
+        printf("2 para atender\n");
+        printf("3 para o proximo \n");
+        scanf("%d",&opcao);
+        
+        switch (opcao)
+        {
+        case 1:
+            printf("digite as pessoas para cadastrar\n");
+            scanf("%d",&p);
+            inserir(&fila,p);
+        break;
+        
+        case 2:
+        atender(&fila);
+        break;
     
-
-// }
-
-// int main()
-// {
+    case 3:
+                proximo(&fila);
+    break;
 
 
-//     tipofila f;
-//     tipoitem item;
+        default:
+        printf("encerrando o programa\n");
+            break;
+        }
 
-//     f.frente = 0;
-//     f.tras = 0;
+    } while (opcao !=0);
+    
+    return 0;
 
-//     cadastrar(&f,&item);
-
-//     return 0;
-// }
+}
